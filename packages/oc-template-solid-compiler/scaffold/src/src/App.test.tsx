0dde9531595f50037e7412cb98a2deb4f48790cf
Ricardo Devis Agullo <mkredz@gmail.com>
2023-04-01 12:56:17 +0200

    update scaffold tests

--- packages/oc-template-solid-compiler/scaffold/src/src/App.test.tsx
+++ packages/oc-template-solid-compiler/scaffold/src/src/App.test.tsx
@@ -1,5 +1,5 @@
 import { describe, it, afterEach, beforeEach, expect, vi } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { render } from '@solidjs/testing-library';
 import { DataProvider } from 'oc-template-solid-compiler/utils/useData';
 
 import App from './App';
@@ -16,13 +16,20 @@ describe('App - Page', () => {
 
   it('Gets more data when clicking the button', () => {
     getData.mockImplementationOnce(() => Promise.resolve({}));
-    render(
-      <DataProvider value={{ firstName: 'firstName', lastName: 'lastName', userId: 0, getData }}>
-        <App firstName="firstName" lastName="lastName" userId={0} />
+    const { getByText } = render(() => (
+      <DataProvider
+        value={{
+          firstName: 'firstName',
+          lastName: 'lastName',
+          userId: 0,
+          getData,
+        }}
+      >
+        <App />
       </DataProvider>
-    );
+    ));
 
-    const extraInfoButton = screen.getByText(/Get extra information/i);
+    const extraInfoButton = getByText(/Get extra information/i);
     expect(extraInfoButton).toBeTruthy();
   });
 });
--- packages/oc-template-solid-compiler/scaffold/src/vite.config.ts
+++ packages/oc-template-solid-compiler/scaffold/src/vite.config.ts
@@ -4,6 +4,15 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   // @ts-ignore Missing test property
   test: {
-    environment: 'jsdom'
-  }
+    environment: 'jsdom',
+    globals: true,
+    transformMode: { web: [/\.[jt]sx?$/] },
+    setupFiles: ['node_modules/@testing-library/jest-dom/extend-expect.js'],
+    // otherwise, solid would be loaded twice:
+    deps: { registerNodeLoader: true },
+    // if you have few tests, try commenting one
+    // or both out to improve performance:
+    threads: false,
+    isolate: false,
+  },
 });
